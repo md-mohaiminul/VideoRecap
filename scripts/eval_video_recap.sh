4b3b6bb63975f52e8a1f680c6b93b30f4f95db56
md-mohaiminul <mmiemon@cs.unc.edu>
2024-03-21 11:24:22 -0400

    extract_caption

--- scripts/eval_video_recap.sh
+++ scripts/eval_video_recap.sh
@@ -17,14 +17,14 @@ python eval.py --metadata datasets/clips_val.pkl --eval_freq 100 \
 #                --batch_size 32 --workers 10
 
 #eval segment descriptions
-CUDA_VISIBLE_DEVICES=1 \
+CUDA_VISIBLE_DEVICES=0 \
 python eval.py --metadata datasets/segments_val.pkl \
                --video_feature_type cls --video_feature_path features/segments \
                --output_dir outputs/segments --resume pretrained_models/videorecap/videorecap_segment.pt \
                --batch_size 32 --workers 10
 
 # eval video summaries
-CUDA_VISIBLE_DEVICES=1 \
+CUDA_VISIBLE_DEVICES=0 \
 python eval.py --metadata datasets/videos_val.json \
                --output_dir outputs/videos --resume pretrained_models/videorecap/videorecap_video.pt \
                --video_feature_type cls --video_feature_path features/videos \
--- scripts/extract_captions.sh
+++ scripts/extract_captions.sh
@@ -1,8 +1,9 @@
+#Use this videos are raw Ego4D videos
 video_feature_path=/data/mmiemon/datasets/ego4d/v1/video_540ss # path to Ego4D videos
 video_encoder_ckpt=pretrained_models/clip_openai_timesformer_base.baseline.ep_0003.pth # path to pretrained video encoder
 resume=pretrained_models/videorecap/videorecap_clip.pt
 feature_step=4 #Extract one caption at each 4 seconds
-CUDA_VISIBLE_DEVICES=1 \
+CUDA_VISIBLE_DEVICES=0 \
 python extract_captions.py --output_dir datasets \
                --video_feature_type pixel --chunk_len -1 --feature_step $feature_step \
                --video_loader_type moviepy \
@@ -10,3 +11,13 @@ python extract_captions.py --output_dir datasets \
                --video_encoder_ckpt $video_encoder_ckpt \
                --resume $resume \
                --batch_size 8 --workers 4
+
+#Use this if videos are cropped to 288px and chunked to 5 minutes (see scripts/crop_and_resize.sh)
+# CUDA_VISIBLE_DEVICES=0 \
+# python extract_captions.py --output_dir datasets \
+#                --video_feature_type pixel --chunk_len 300 --feature_step $feature_step \
+#                --video_loader_type decord \
+#                --video_feature_path $video_feature_path \
+#                --video_encoder_ckpt $video_encoder_ckpt \
+#                --resume $resume \
+#                --batch_size 8 --workers 4
